58252b42b68ef682bde7e2ebb77db1265a9cc50b
Aaron Scott <ascott@comfyrainbow.com>
2023-06-22 20:07:49 -1000

    added documentation

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #bin/bash
 
+# this is used to validate working with openssl encryption
+# you can safely ignore it unless you want to try it too
+
 # load the passkey variable from .env file
 source .env
 
 # then encrypt the file using aes-256-cbc no IV
 openssl enc -aes-256-cbc -in backup.sh -out backup.enc -pass pass:$JOURNAL_ENCRYPTION_KEY
 openssl enc -d -aes-256-cbc -in backup.enc -out decrypted-backup.sh -pass pass:$JOURNAL_ENCRYPTION_KEY
-
 
 # test with IV
 # get a random hex string for IV
@@ -24,4 +26,3 @@ source .env
 
 # decrypt the file
 openssl enc -d -aes-256-cbc -iv $IV -in backup-iv.enc -out decrypted-iv-backup.sh -pass pass:$JOURNAL_ENCRYPTION_KEY
-
